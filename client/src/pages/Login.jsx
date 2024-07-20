55dec16821afa24406aa47c7c68e1a1b50f68c5d
Hafiz Muahammad Abdullah <76812283+Cleverprogrammer-69@users.noreply.github.com>
2024-07-21 02:05:03 +0500

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,23 @@
 import { useLogin } from "../hooks/useLogin";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { login, error, isLoading } = useLogin();
+  const navigate = useNavigate(); // Hook to programmatically navigate
+
   const SubmitHandler = async (e) => {
     e.preventDefault();
-    await login(email, password);
-    console.log( email, password);
+    try {
+      await login(email, password);
+      navigate("/feed"); // Redirect to feed page on successful login
+    } catch (err) {
+      console.error(err); // Handle error if login fails
+    }
   };
+
   return (
     <div>
       <form className="login" onSubmit={SubmitHandler}>
@@ -29,7 +37,7 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <p id="LoginMsg">
-          Not a Member <Link to="/signup">Register here</Link>{" "}
+          Not a Member <Link to="/signup">Register here</Link>
         </p>
         <button disabled={isLoading} type="submit">
           Login
